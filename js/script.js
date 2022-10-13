96827232a10ebdd4b1ba4bc2454e1de2f1d2008a
Thunderbird93 <antircristian@gmail.com>
2022-10-13 16:24:33 +0200

    fin js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 
 for (let i = 1; i < 101; i++){
+
+    if (i % 3 === 0){
+        console.log("Fizz")
+    }
+
+    if (i % 5 === 0){
+        console.log("Buzz")
+    }
+
+    if (i % 15 === 0){
+        console.log("FizzBuzz")
+    }
+
     console.log(i);
 }
 
